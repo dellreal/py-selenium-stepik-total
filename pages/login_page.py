0c937817f9cc84b71177541efb7727c1647b03fa
dell_real <evgtalanov@gmail.com>
2022-11-24 22:40:31 +0300

    repair method should_be_login_url

--- pages/login_page.py
+++ pages/login_page.py
@@ -9,7 +9,7 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        assert "/login" in self.open(), "Login url is invalid"
+        assert "/login" in self.url, "Login url is invalid"
         # assert link != self.url, "Login url is invalid"
 
     def should_be_login_form(self):
