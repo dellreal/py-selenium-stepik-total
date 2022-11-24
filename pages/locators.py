31bb19f4e459cbcf9a83df5123d143f36664a539
dell_real <evgtalanov@gmail.com>
2022-11-24 16:29:36 +0300

    relocate methods to base_page

--- pages/locators.py
+++ pages/locators.py
@@ -1,10 +1,6 @@
 from selenium.webdriver.common.by import By
 
 
-class MainPageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-
-
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
@@ -17,3 +13,7 @@ class ProductPageLocators():
     PRODUCT_NAME_IN_CART = (By.CSS_SELECTOR, "#messages strong")
     PRODUCT_PRICE_IN_CART = (By.CSS_SELECTOR, "p.price_color")
     SUCCESS_MESSAGE = (By.CSS_SELECTOR, '#messages .alertinner strong')  # поправить селектор
+
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
--- test_product_page.py
+++ test_product_page.py
@@ -1,27 +1,27 @@
 from pages.product_page import ProductPage
 import pytest
 
-# url_list = ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-#             "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-#             "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-#             "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-#             "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-#             "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-#             "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-#             pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
-#             "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
-#             "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"]
+url_list = ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+            "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+            "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+            "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+            "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+            "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+            "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+            pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
+            "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+            "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"]
 
 
-# @pytest.mark.parametrize('url', url_list)
-# def test_add_to_cart(browser, url):
-#     link = url
-#     page = ProductPage(browser, link)
-#     page.open()
-#     page.should_be_add_to_cart_button()
-#     page.add_to_cart()
-#     page.should_be_correct_adding_product_price()
-#     page.should_be_correct_adding_product_name()
+@pytest.mark.parametrize('url', url_list)
+def test_add_to_cart(browser, url):
+    link = url
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_add_to_cart_button()
+    page.add_to_cart()
+    page.should_be_correct_adding_product_price()
+    page.should_be_correct_adding_product_name()
 
 @pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
@@ -38,6 +38,7 @@ def test_guest_cant_see_success_message(browser):
     page.open()
     page.should_not_be_success_message()
 
+
 @pytest.mark.xfail
 def test_message_disappeared_after_adding_product_to_basket(browser):
     link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
@@ -45,6 +46,20 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.open()
     page.add_to_cart()
     page.should_see_as_disppearing_message()
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
 
 # Command to start
 # pytest -s test_product_page.py
